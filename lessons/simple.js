f3460a4e83cc11381eea3eeb2321faba1fc9a058
denidoman@gmail.com <Born91>
2020-02-18 18:20:20 +0300

    omg! I just write jest!

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,32 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require("../math");
 
-let result, expected
+test("Sum should be ok", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
-result = sum(3, 7)
-expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+test("Subtraction should be ok", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`PASS: '${title}'`);
+  } catch (error) {
+    console.error(`FAIL: '${title}'. Reason: ${error}`);
+  }
 }
 
-result = subtract(7, 3)
-expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual != expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    }
+  };
 }
